44a1129849aa3777f93ab5f0cd6b8bd6df7082ce
Collins Wakahenya <wakahenyacollins@gmail.com>
2023-06-20 02:32:36 +0300

    Done with assignment

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const divide=() => {
+    return 2000/100
+} 
+ divide();
+
+ const square = (num) => {
+    return num * num;
+  };
+  
+  const add = (num1, num2) =>  {
+    let num1Value = 0; 
+    let num2Value = 7;
+    return num1Value + num2Value; 
+  };
+  
+
+  
+    
+  
